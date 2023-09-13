788c65709b695517e3cf6a9150a1af66473fc0e1
joy935 <tupaiv@gmail.com>
2023-09-13 14:50:11 +0200

    added function setusername

--- beginner-html-site-styled-gh-pages/scripts/main.js
+++ beginner-html-site-styled-gh-pages/scripts/main.js
@@ -11,3 +11,10 @@ myImage.onclick = () => {
 
 let myButton = document.querySelector("button");
 let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Mozilla is cool, ${myName}`;
+  }
+  
