259cd5361f1b2662ee2e99b0bda10ce87cfc7c34
joy935 <tupaiv@gmail.com>
2023-09-13 12:22:43 +0200

    changed extension image file

--- beginner-html-site-styled-gh-pages/scripts/main.js
+++ beginner-html-site-styled-gh-pages/scripts/main.js
@@ -3,7 +3,7 @@ const myImage = document.querySelector("img");
 myImage.onclick = () => {
   const mySrc = myImage.getAttribute("src");
   if (mySrc === "images/firefox-icon.png") {
-    myImage.setAttribute("src", "images/firefox2.png");
+    myImage.setAttribute("src", "images/firefox2.jpg");
   } else {
     myImage.setAttribute("src", "images/firefox-icon.png");
   }
