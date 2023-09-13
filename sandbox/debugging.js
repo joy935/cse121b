7a610e2c299e7d10f0c1677d69114b06a9586a15
joy935 <tupaiv@gmail.com>
2023-09-13 16:11:48 +0200

    added function circleArea

--- sandbox/debugging.js
+++ sandbox/debugging.js
@@ -1,6 +1,10 @@
 const PI = 3.14;
 let radius = 3;
 let area = 0;
+
+function circleArea(radius) {
+    radius * radius * PI
+}
 area = radius * radius * PI;
 console.log("area1:", area)
 radius = 4;
