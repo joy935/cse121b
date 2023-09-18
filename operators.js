08a6a53d832d7e71a147f1fdac5f7f33b842b767
joy935 <tupaiv@gmail.com>
2023-09-18 11:59:14 +0200

    added incrementation

--- operators.js
+++ operators.js
@@ -1,27 +1,34 @@
-const x = 1
-const y = 2
+const x = 1;
+const y = 2;
 
 // Add two numbers
-let a = x + y
-console.log(a)
+let a = x + y;
+console.log(a);
 
 // Multiply two numbers
-a = x * y
-console.log(a)
+a = x * y;
+console.log(a);
 
 // Subtract one from a number
-a = x - y
-console.log(a)
+a = x - y;
+console.log(a);
 
 // concatinate two strings together.
-const b = "Good "
-const c = "morning"
-console.log(b+c)
+const b = "Good ";
+const c = "morning";
+console.log(b+c);
 
 // assign a value to a variable
-a = 10
-console.log(a)
+a = 10;
+console.log(a);
+
 // increment the value in a variable by 3
+let d = 0;
+for (let i = 0; i < 5; i++) {
+    d += 3;
+    console.log(d);
+}
+
 // compare two values to see if they are the same
 // check to see if one number is less than another number
 // Check to see if two values are NOT equal
