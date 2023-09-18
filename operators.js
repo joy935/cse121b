ff49f7d9e2b171ac32915f2f62313ba243011587
joy935 <tupaiv@gmail.com>
2023-09-18 11:15:16 +0200

    new js file

---
+++ operators.js
@@ -0,0 +1,15 @@
+x = 1
+y = 2
+
+// Add two numbers
+let a = x + y
+console.log(a)
+// Multiply two numbers
+// Subtract one from a number
+// concatinate two strings together.
+// assign a value to a variable
+// increment the value in a variable by 3
+// compare two values to see if they are the same
+// check to see if one number is less than another number
+// Check to see if two values are NOT equal
+// check to see if a value is less than 10 and greater than 0
