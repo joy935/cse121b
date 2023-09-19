79f3c4bcd8b74f973caacc9913fb5a8ab8e8da6d
joy935 <tupaiv@gmail.com>
2023-09-19 10:13:57 +0200

    added a list

--- dom-basics.js
+++ dom-basics.js
@@ -6,3 +6,7 @@ const newImage = document.createElement("img");
 newImage.setAttribute("src", "https://picsum.photos/200");
 newImage.setAttribute("alt", "new-img");
 document.body.appendChild(newImage);
+
+const newDiv = document.createElement("div");
+newDiv.innerHTML = "<ul><li>One</li><li>Two</li><li>Three</li></ul>";
+document.body.appendChild(newDiv);
