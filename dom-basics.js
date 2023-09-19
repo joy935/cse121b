21ee35b16645a565d652783780d8ddfc21a6078e
joy935 <tupaiv@gmail.com>
2023-09-19 10:13:22 +0200

    added a new image

--- dom-basics.js
+++ dom-basics.js
@@ -1,3 +1,8 @@
 const newParagraph = document.createElement("p");
 newParagraph.innerText = "Added with Javascript!";
 document.body.appendChild(newParagraph);
+
+const newImage = document.createElement("img");
+newImage.setAttribute("src", "https://picsum.photos/200");
+newImage.setAttribute("alt", "new-img");
+document.body.appendChild(newImage);
