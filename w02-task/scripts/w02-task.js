a5f0c96c7915e5defd206d6d3ccf7d1422853a37
joy935 <tupaiv@gmail.com>
2023-09-19 13:34:00 +0200

    added array and adjusted image

--- w02-task/scripts/w02-task.js
+++ w02-task/scripts/w02-task.js
@@ -4,28 +4,40 @@
 
 /* Step 2 - Variables */
 const fullName = "Veihi Tupai";
-const currentYear = 2023;
-const profilePicture = "w02-task/images/photo_veihi.pdf";
+const currentYear = "2023";
+var profilePicture = "images/photo_veihi.png";
 
 
 /* Step 3 - Element Variables */
-const nameElement = document.getElementById("#name");
-const foodElement = document.getElementById("#food");
-const yearElement = document.getElementById("#year");
-const imageElement = document.querySelector("images/placeholder.png").scr;
+const nameElement = document.getElementById("name");
+const foodElement = document.getElementById("food");
+const yearElement = document.querySelector("#year");
+var imageElement = document.querySelector("img");
 
 
 /* Step 4 - Adding Content */
 nameElement.innerHTML = `<strong>${fullName}</strong>`;
 yearElement.textContent = currentYear;
 imageElement.setAttribute("src", profilePicture);
-imageElement.setAttribute("alt", "Profile image of [fullName]");
+imageElement.setAttribute("alt", "Profile image of ${fullName}");
 
 
 /* Step 5 - Array */
+let favoriteFood = ["Sushi", "Mango", "Steak frites", "Raspberries", "Chocolate"];
+foodElement.textContent = favoriteFood.join(', ');
+
+const newFavoriteFood = "Pizza"; 
+favoriteFood.push(newFavoriteFood);
+foodElement.innerHTML += `<br>${favoriteFood.join(', ')}`;
+
+favoriteFood.shift();
+foodElement.innerHTML += `<br>${favoriteFood.join(', ')}`;
+
+favoriteFood.pop();
+foodElement.innerHTML += `<br>${favoriteFood.join(', ')}`;
 
 
-
-
-
-
+/* Adjust the image */
+imageElement.style.maxWidth = '200px';
+imageElement.style.borderRadius = '10px';
+imageElement.style.boxShadow = '4px 4px 8px rgba(0, 0, 0, 0.8)'; 
