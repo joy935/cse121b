3f193ce8d7af2e17e0ef6b9f90a849774857ef8b
joy935 <tupaiv@gmail.com>
2023-09-22 13:57:37 +0200

    added array methods

--- practice/array-methods.js
+++ practice/array-methods.js
@@ -1,1 +1,16 @@
 let names = ['Nancy','Blessing','Jorge','Svetlana','Bob'];
+
+// function firstLetter(names, letter) {
+//     for (let i; names.lenght(), i++) {
+//         if (names[i][0] === letter) {
+//             return names[i];
+//         }
+//     }
+// }
+// let namesB = console.log(filter(names, "B"));
+
+let namesB = names.filter(name => name.charAt(0) === "B");
+
+let namesLengths = names.map((name) => name.length);
+
+const averageLength = names.reduce((total, names) => total + name.length, 0) / names.length;
