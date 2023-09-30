4f7442bc0355356cf517f3281120ea73391a12c4
joy935 <tupaiv@gmail.com>
2023-09-30 16:44:48 +0200

    added enroll and drop methods

--- practice/w4practiceobjects.js
+++ practice/w4practiceobjects.js
@@ -10,23 +10,42 @@ const aCourse = {
             sectionNum: 2, roomNum: "STC 347", enrolled: 28, days: "TTh", instructor: "Sis A"
         },
     ],
-    };
+    enrollStudent: function(sectionNum) {
+        const sectionIndex = this.sections.findIndex((section) => section.sectionNum == sectionNum);
+        if (sectionIndex >= 0) {
+            this.sections[sectionIndex].enrolled++;
+            renderSections(this.sections);
+        }
+    },
 
-    function setCourseInfo(course){
-        document.getElementById("courseName").textContent = course.name;
-        document.getElementById("courseCode").textContent = course.code;
-    };
+    dropStudent: function(sectionNum) {
+        const sectionIndex = this.sections.findIndex((section) => section.sectionNum == sectionNum);
+        if (sectionIndex >= 0) {
+            this.sections[sectionIndex].enrolled--;
+            renderSections(this.sections);
+        }
+    },
+};
 
-    function renderSections(sections) {
-        const html = sections.map((section) => `<tr>
-        <td>${section.sectionNum}</td>
-        <td>${section.roomNum}</td>
-        <td>${section.enrolled}</td>
-        <td>${section.days}</td>
-        <td>${section.instructor}</td>
-        </tr>`);
-        document.getElementById("sections").innerHTML = html;
-    };
+function setCourseInfo(course){
+    document.getElementById("courseName").textContent = course.name;
+    document.getElementById("courseCode").textContent = course.code;
+};
 
-  setCourseInfo(aCourse);
-  renderSections(aCourse.sections);
+function renderSections(sections) {
+    const html = sections.map((section) => `<tr>
+    <td>${section.sectionNum}</td>
+    <td>${section.roomNum}</td>
+    <td>${section.enrolled}</td>
+    <td>${section.days}</td>
+    <td>${section.instructor}</td>
+    </tr>`);
+    document.getElementById("sections").innerHTML = html;
+};
+
+document.getElementById("enrollStudent").addEventListener("click", function() {const sectionNum = document.getElementById("sectionNumber").value; aCourse.enrollStudent(sectionNum)});
+
+document.getElementById("dropStudent").addEventListener("click", function() {const sectionNum = document.getElementById("sectionNumber").value; aCourse.dropStudent(sectionNum)});
+
+setCourseInfo(aCourse);
+renderSections(aCourse.sections);
