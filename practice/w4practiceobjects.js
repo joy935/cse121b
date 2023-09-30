2811fd2836102560b55ae143630c04263970e0fa
joy935 <tupaiv@gmail.com>
2023-09-30 12:56:05 +0200

    added object and functions

--- practice/w4practiceobjects.js
+++ practice/w4practiceobjects.js
@@ -2,4 +2,31 @@
 const aCourse = {
     code: "CSE121b",
     name: "Javascript Language",
-  };
+    sections: [
+        { 
+            sectionNum: 1, roomNum: "STC 353", enrolled: 26, days: "TTh", instructor: "Bro T" 
+        },
+        { 
+            sectionNum: 2, roomNum: "STC 347", enrolled: 28, days: "TTh", instructor: "Sis A"
+        },
+    ],
+    };
+
+    function setCourseInfo(course){
+        document.getElementById("courseName").textContent = course.name;
+        document.getElementById("courseCode").textContent = course.code;
+    };
+
+    function renderSections(sections) {
+        const html = sections.map((section) => `<tr>
+        <td>${section.sectionNum}</td>
+        <td>${section.roomNum}</td>
+        <td>${section.enrolled}</td>
+        <td>${section.days}</td>
+        <td>${section.instructor}</td>
+        </tr>`);
+        document.getElementById("sections").innerHTML = html;
+    };
+
+  setCourseInfo(aCourse);
+  renderSections(aCourse.sections);
