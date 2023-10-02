a3fe96abdd18840d6f3ed58d8c24951ee7e62339
joy935 <tupaiv@gmail.com>
2023-10-02 10:06:44 +0200

    added name dom

--- scripts/w04-task.js
+++ scripts/w04-task.js
@@ -1,17 +1,42 @@
 /* LESSON 3 - Programming Tasks */
 
 /* Profile Object  */
-
-
-
+let myProfile = {
+    name: "Veihi Tupai",
+    photo: {
+        imagePath: "images/photo_veihi.png",
+        imageName: "Photo Veihi"},
+    favoriteFoods: [ 
+        "Sushi", 
+        "Mango", 
+        "Steak Frites", 
+        "Raspberries", 
+        "Chocolate"],
+    hobbies: [
+        "Reading",
+        "Cooking",
+        "Travelling"],
+    placesLived: [],
+};
 
 /* Populate Profile Object with placesLive objects */
-
-
-
+myProfile.placesLived.push(
+    {
+        place: "Saskatoon, SK, Canada",
+        length: "4 years"
+    },
+    {
+        place: "Geneva, GE, Switzerland",
+        length: "1 year"
+    },
+    {
+        place: "Brugg, AG, Switzerland",
+        length: "2 months"
+    },
+);
 
 /* DOM Manipulation - Output */
-
+document.getElementById("name").textContent = myProfile.name;
 /* Name */
 
 /* Photo with attributes */
