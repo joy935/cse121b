2191ea796ab0ccd895f27bbe1500cd928837daf0
joy935 <tupaiv@gmail.com>
2023-10-02 11:25:27 +0200

    added places lived

--- scripts/w04-task.js
+++ scripts/w04-task.js
@@ -22,8 +22,16 @@ let myProfile = {
 /* Populate Profile Object with placesLive objects */
 myProfile.placesLived.push(
     {
+        place: "Tahiti, French Polynesia",
+        length: "22 years"
+    },
+    {
+        place: "Paris, France",
+        length: "2.5 years"
+    },
+    {
         place: "Saskatoon, SK, Canada",
-        length: "4 years"
+        length: "5 years"
     },
     {
         place: "Geneva, GE, Switzerland",
@@ -62,5 +70,14 @@ myProfile.hobbies.forEach (hobby => {
 })
 
 /* Places Lived DataList */
+const placeLived = document.querySelector("#places-lived");
+myProfile.placesLived.forEach ((item) => {
+    let list1 = document.createElement("dt");
+    let list2 = document.createElement("dd");
 
+    list1.textContent = item.place;
+    list2.textContent = item.length;
 
+    placeLived.appendChild(list1);
+    placeLived.appendChild(list2);
+})
