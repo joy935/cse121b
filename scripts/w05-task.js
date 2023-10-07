e3f7eee012a046fa47d7751ba0beeea2c317fc80
joy935 <tupaiv@gmail.com>
2023-10-07 14:46:25 +0200

    added fetch

--- scripts/w05-task.js
+++ scripts/w05-task.js
@@ -5,12 +5,26 @@ const templesElement = document.querySelector("#temples");
 let templeList = [];
 
 /* async displayTemples Function */
-
-
+const displayTemples = (temples) => {
+    temples.forEach(temple => {
+       let temple = document.createElement("ARTICLE");
+       let templeName = document.createElement("h3");
+       oneTemple.appendChild(templeName);
+       let templeImage = document.createElement("IMG"); 
+       templeImage.setAttribute("src", imageUrl);
+       templeImage.setAttribute("alt", location);
+       oneTemple.appendChild(templeName);
+       templesElement.appendChild(oneTemple);
+    });
+};
 
 
 /* async getTemples Function using fetch()*/
-
+const getTemples = async () => {
+    const response = await fetch("https://byui-cse.github.io/cse121b-ww-course/resources/temples.json");
+    const templeList = await response.json();
+    displayTemples(templeList);
+};
 
 /* reset Function */
 
@@ -20,5 +34,6 @@ let templeList = [];
 
 
 getTemples();
+console.log(templeList);
 
 /* Event Listener */
