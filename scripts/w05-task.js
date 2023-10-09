13136c9f0967f23401a587c059b59fb06edf558c
joy935 <tupaiv@gmail.com>
2023-10-09 11:41:48 +0200

    added filter

--- scripts/w05-task.js
+++ scripts/w05-task.js
@@ -6,33 +6,55 @@ let templeList = [];
 
 /* async displayTemples Function */
 const displayTemples = (temples) => {
-    temples.forEach(temple => {
-       let temple = document.createElement("ARTICLE");
+    temples.forEach((temple) => {
+       let oneTemple = document.createElement("ARTICLE");
        let templeName = document.createElement("h3");
        oneTemple.appendChild(templeName);
        let templeImage = document.createElement("IMG"); 
-       templeImage.setAttribute("src", imageUrl);
-       templeImage.setAttribute("alt", location);
-       oneTemple.appendChild(templeName);
+       templeImage.setAttribute("src", temple.imageUrl);
+       templeImage.setAttribute("alt", temple.location);
+       oneTemple.appendChild(templeImage);
        templesElement.appendChild(oneTemple);
     });
 };
 
-
 /* async getTemples Function using fetch()*/
 const getTemples = async () => {
     const response = await fetch("https://byui-cse.github.io/cse121b-ww-course/resources/temples.json");
-    const templeList = await response.json();
+    templeList = await response.json();
     displayTemples(templeList);
 };
 
 /* reset Function */
-
+const reset = () => {
+        templesElement.innerHTML = ""};
 
 /* sortBy Function */
+const sortBy = (temple) => {
+    reset();
+    let filter = document.getElementById("sortBy").value;
+    switch (filter) {
+        case "utah":
+            const utah = templeList.filter((temples) => temples.location.includes("Utah"));
+            displayTemples(utah);
+            break;
+        case "notutah":
+            const notutah = templeList.filter((temples) => !temples.location.includes("Utah"));
+            displayTemples(notutah);
+            break;
+        case "older":
+            const older = templeList.filter((temples) => new Date(temples.dedicated) < new Date(1950, 0, 1));
+            displayTemples(older);
+            break;
+        case "all":
+            displayTemples(templeList);
+            break;
+        default:
+            console.log("Choose a filter");
+    };
+};
 
-
-
+document.querySelector("#sortBy").addEventListener("change", () => {sortBy(templeList)});
 getTemples();
 console.log(templeList);
 
