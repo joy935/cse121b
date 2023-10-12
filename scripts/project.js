d1027a4385eed636da1ee860e28ad3598eac55ee
joy935 <tupaiv@gmail.com>
2023-10-12 14:01:01 +0200

    changed project to emoji

--- scripts/project.js
+++ scripts/project.js
@@ -10,15 +10,24 @@ const displayEmoji = (emojis) => {
         let article = document.createElement("article");
         let emojiName = document.createElement("h3");
         emojiName.textContent = emoji.name;
-
+        let emojiHtml = document.createElement("p");
+        emojiHtml.innerHTML = emoji.htmlCode;
+        let emojiCategory = document.createElement("p");
+        emojiCategory.textContent = emoji.category;
+        let emojiGroup = document.createElement("p");
+        emojiGroup.textContent = emoji.group;
+        
         article.appendChild(emojiName);
+        article.appendChild(emojiHtml);
+        article.appendChild(emojiCategory);
+        article.appendChild(emojiGroup);
         emojiElement.appendChild(article);
     })};
 /* async getFruits Funtion using fetch() */
 const getEmoji = async () => {
     const response = await fetch("https://emojihub.yurace.pro/api/all");
     emojiList = await response.json();
-    //displayMeteo(emojiList);
+    displayEmoji(emojiList);
     console.log(emojiList);
 };
 
