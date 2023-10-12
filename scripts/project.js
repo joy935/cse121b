da389ade7993cccf750fc8cf3a10ebae1276d1f3
joy935 <tupaiv@gmail.com>
2023-10-12 13:32:17 +0200

    chnaged project to weather forecast

--- scripts/project.js
+++ scripts/project.js
@@ -1,45 +1,26 @@
 /* W07: Project */
 
 /* Declare and initialize global variables */
-const fruitElement = document.querySelector("#fruits");
-let fruitList = [];
+const meteoElement = document.querySelector("#meteo");
+let meteoList = [];
 
 /* async displayFruits Function */
-const displayFruits = (fruits) => {
-    fruits.forEach((fruit) => {
+const displayMeteo = (meteo) => {
+    meteo.forEach((meteo) => {
         let article = document.createElement("article");
         let fruitName = document.createElement("h3");
         fruitName.textContent = fruit.name;
-        let fruitId = document.createElement("p");
-        fruitId.textContent = fruit.id;
-        let fruitGenus = document.createElement("p");
-        fruitGenus.textContent = fruit.genus;
-        let fruitFamily = document.createElement("p");
-        fruitFamily.textContent = fruit.family;
-        let fruitOrder = document.createElement("p");
-        fruitOrder.textContent = fruit.order;
-        let fruitNutrition = document.createElement("p");
-        fruitNutrition.textContent = fruit.nutrition;
 
         article.appendChild(fruitName);
-        article.appendChild(fruitId);
-        article.appendChild(fruitGenus);
-        article.appendChild(fruitFamily);
-        article.appendChild(fruitOrder);
-        article.appendChild(fruitNutrition);
-        fruitElement.appendChild(article);
+        meteoElement.appendChild(article);
     })};
 /* async getFruits Funtion using fetch() */
-const getFruits = async () => {
-    const response = await fetch("https://www.fruityvice.com/api/fruit/all");
-    fruitList = await response.json();
-    displayFruits(fruitList);
-    console.log(fruitList);
+const getMeteo = async () => {
+    const response = await fetch("https://api.weather.gov/");
+    meteoList = await response.json();
+    displayMeteo(meteoList);
+    console.log(meteoList);
 };
 
 /* */
-
-// document.querySelector("#search").addEventListener("click", function () {
-//    // let fruit_name = document.querySelector("#fruitName").value;
-//     getFruits()});
 getFruits();
