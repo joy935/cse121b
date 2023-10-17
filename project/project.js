79841e3de30da9ccc09b44853c637cd9ba9203cc
joy935 <tupaiv@gmail.com>
2023-10-17 09:59:07 +0200

    added font

--- project/project.js
+++ project/project.js
@@ -10,6 +10,9 @@ const displayEmoji = (emojis) => {
     emojis.forEach((emoji) => {
         let li = document.createElement("li");
         li.setAttribute("emojiName", emoji.name);
+        // Extract the hexadecimal part and convert it to decimal
+        // const unicodePoint = parseInt((emoji.unicode).slice(2), 16);
+        // li.innerHTML = String.fromCodePoint(unicodePoint);
         li.innerHTML = emoji.htmlCode;
         li.setAttribute("category", emoji.category);
         emojiElement.appendChild(li);
@@ -22,8 +25,8 @@ const getEmoji = async () => {
         const response = await fetch("https://emojihub.yurace.pro/api/all");
         emojiList = await response.json();
         displayEmoji(emojiList);
-    } catch (err) {
-        console.log("Error fetching emoji data: ", err);
+    } catch (error) {
+        console.log("Error fetching emoji data: ", error);
     }
 };
 
